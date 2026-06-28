5fa72a9c2bb555195ff01f4e4eb300c74dbff95e
EDITH2007 <230107anu@gmail.com>
2026-06-28 15:30:42 +0530

    dpeloyment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src", // Vite understands this is relative to your project root
+    },
+  },
 })
